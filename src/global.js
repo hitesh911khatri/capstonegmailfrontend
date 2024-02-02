d83bfc7133c3c4a51de63e8de2c3a6b5a768bd2c
hitesh911khatri <149157855+hitesh911khatri@users.noreply.github.com>
2024-02-02 14:41:13 +0530

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,3 +1,4 @@
 //export const API = "http://localhost:4000"
 //export const API = "https://gmailclone-backend.onrender.com"
-export const API = "https://gmailbackendclonecapstone.vercel.app"
+// export const API = "https://gmailbackendclonecapstone.vercel.app"
+export const API = "https://gmailclone-backend-e7tj.onrender.com"
